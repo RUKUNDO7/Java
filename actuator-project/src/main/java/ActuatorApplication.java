217b3832e5353e17570ed5a865c8ef4103552c7d
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-10 21:34:15 +0200

    Actuator main file

---
+++ actuator-project/src/main/java/ActuatorApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.actuator.ActuatorApplication.class, args);
+    }
+}
