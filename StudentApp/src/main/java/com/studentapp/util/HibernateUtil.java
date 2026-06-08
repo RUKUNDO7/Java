fd19cbb823ff23c3febdf620d3a9fd115dbd76d1
Rukundo <gihozoRukundobenise@gmail.com>
2026-06-08 09:19:19 +0200

    Hibernate util class

---
+++ StudentApp/src/main/java/com/studentapp/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package com.studentapp.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Load hibernate.cfg.xml from classpath (WEB-INF/classes)
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+}
