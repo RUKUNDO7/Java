8090647ab2f39d725d7830fabc054c5adcc7c668
Rukundo <gihozoRukundobenise@gmail.com>
2026-06-10 10:22:45 +0200

    Student class for fifth exercise

---
+++ Transitioning/OOP/FifthExercise/src/Student.java
@@ -0,0 +1,17 @@
+class Student {
+    public String name;
+    public int grade;
+
+    Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getStatus() {
+        if (grade >= 50) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
+}
